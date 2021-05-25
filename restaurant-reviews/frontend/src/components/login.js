5938c57884cb540c2cd8e18827953d6181fe313f
Muhammad Zuhdi <Muhammad.Zuhdi07@gmail.com>
2021-05-24 22:05:55 -0400

    added components strcuture

---
+++ restaurant-reviews/frontend/src/components/login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+function Login() {
+  return (
+    <div className="App">
+      Login page
+    </div>
+  );
+}
+
+export default Login;
---
+++ restaurant-reviews/frontend/src/components/restaurants.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+function Restaurant() {
+  return (
+    <div className="App">
+      Restaurant page
+    </div>
+  );
+}
+
+export default Restaurant;
