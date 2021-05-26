d953b5412634503fbce26d5cd766c5d5cf0f7292
Muhammad Zuhdi <Muhammad.Zuhdi07@gmail.com>
2021-05-25 22:02:37 -0400

    added more restaurant list frontend code

--- restaurant-reviews/backend/api/restaurants.route.js
+++ restaurant-reviews/backend/api/restaurants.route.js
@@ -7,7 +7,7 @@ const router = express.Router()
 router.route("/").get(RestaurantsCtrl.apiGetRestraurants)
 router.route("/id/:id").get(RestaurantsCtrl.apiGetRestraurantById)
 router.route("review/id/:id").get(RestaurantsCtrl.apiGetRestraurantByIdReview)
-router.route("/cuisine").get(RestaurantsCtrl.apiGetRestraurantsCuisines)
+router.route("/cuisines").get(RestaurantsCtrl.apiGetRestraurantsCuisines)
 
 router
     .route("/review")
